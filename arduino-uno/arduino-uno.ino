23cf0e63a3f63d5cbfb08a6673b83fcb12b78e54
Miccah Castorina <m.castorina93@gmail.com>
2020-09-06 21:35:06 -0500

    Arduino POC
    This sketch uses a potentiometer to calculate the warmth value as a
    function of hue. Hue is then converted to RGB, and a library writes the
    data to the LED strip.

---
+++ arduino-uno/arduino-uno.ino
@@ -0,0 +1,90 @@
+#include <PololuLedStrip.h>
+
+int getHueFromSensor();
+void hsv2rgb(unsigned char*, unsigned char*, unsigned char*, float, float, float);
+
+// Create an ledStrip object and specify the pin it will use.
+PololuLedStrip<12> ledStrip;
+int potPin = A0;    // select the input pin for the potentiometer
+
+// Create a buffer for holding the colors (3 bytes per color).
+#define LED_COUNT 60
+rgb_color colors[LED_COUNT];
+
+void setup() {}
+
+void loop() {
+    unsigned int h = getHueFromSensor();
+    rgb_color color;
+    hsv2rgb(&color.red, &color.green, &color.blue, h, 1, 1);
+
+    // Update the colors buffer.
+    for(uint16_t i = 0; i < LED_COUNT; i++) {
+        colors[i] = color;
+    }
+
+    // Write to the LED strip.
+    ledStrip.write(colors, LED_COUNT);
+    delay(100);
+}
+
+int getHueFromSensor() {
+    // read the value from the sensor:
+    long sensorValue = analogRead(potPin);
+    long h = sensorValue * 6 / 25 + 120;
+    if (h >= 360) {
+        h = 0;
+    }
+    return (int)h;
+}
+
+void hsv2rgb(unsigned char *r, unsigned char *g, unsigned char *b, float h, float s, float v) {
+    int i;
+    float f, p, q, t;
+
+    if (s == 0) {
+        // achromatic (grey)
+        *r = *g = *b = 255 * v;
+        return;
+    }
+
+    h /= 60;
+    i = floor(h);
+    f = h - i;
+    p = v * ( 1 - s );
+    q = v * ( 1 - s * f );
+    t = v * ( 1 - s * ( 1 - f ) );
+
+    switch (i) {
+        case 0:
+            *r = 255 * v;
+            *g = 255 * t;
+            *b = 255 * p;
+            break;
+        case 1:
+            *r = 255 * q;
+            *g = 255 * v;
+            *b = 255 * p;
+            break;
+        case 2:
+            *r = 255 * p;
+            *g = 255 * v;
+            *b = 255 * t;
+            break;
+        case 3:
+            *r = 255 * p;
+            *g = 255 * q;
+            *b = 255 * v;
+            break;
+        case 4:
+            *r = 255 * t;
+            *g = 255 * p;
+            *b = 255 * v;
+            break;
+        default:
+            *r = 255 * v;
+            *g = 255 * p;
+            *b = 255 * q;
+            break;
+    }
+}
